0ad943a79fb2c8140f51cb2745276ea2b236a820
William <williamyip1112@gmail.com>
2018-12-06 09:38:35 -0800

    Fixed a bug

--- app/src/main/java/com/per2/cryptolearn/caesarLearnFragment.java
+++ app/src/main/java/com/per2/cryptolearn/caesarLearnFragment.java
@@ -1,4 +1,27 @@
 package com.per2.cryptolearn;
 
-public class caesarLearnFragment extends android.support.v4.app.Fragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class caesarLearnFragment extends android.support.v4.app.Fragment
+{
+    private TextView article;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_caesar_learn, container, false);
+        wireWidgets(rootView);
+        return rootView;
+    }
+
+    public void wireWidgets(View view)
+    {
+        article = view.findViewById(R.id.textView_main_article);
+    }
+}
 }
